21a68bff5af1ec45538a8547891c53c58b9ee1b8
Paul Andrews <judge2005@gmail.com>
2020-12-19 22:57:23 -0500

    Free up heap by not initializing BLE stack

--- src/A2DPAudio.cpp
+++ src/A2DPAudio.cpp
@@ -16,13 +16,56 @@
 
 #define A2DP_AUDIO_TAG	"A2DP_AUDIO"
 
+#define BT_CONTROLLER_CLASSIC_CONFIG() {                              \
+    .controller_task_stack_size = ESP_TASK_BT_CONTROLLER_STACK,            \
+    .controller_task_prio = ESP_TASK_BT_CONTROLLER_PRIO,                   \
+    .hci_uart_no = BT_HCI_UART_NO_DEFAULT,                                 \
+    .hci_uart_baudrate = BT_HCI_UART_BAUDRATE_DEFAULT,                     \
+    .scan_duplicate_mode = SCAN_DUPLICATE_MODE,                            \
+    .scan_duplicate_type = SCAN_DUPLICATE_TYPE_VALUE,                      \
+    .normal_adv_size = NORMAL_SCAN_DUPLICATE_CACHE_SIZE,                   \
+    .mesh_adv_size = MESH_DUPLICATE_SCAN_CACHE_SIZE,                       \
+    .send_adv_reserved_size = SCAN_SEND_ADV_RESERVED_SIZE,                 \
+    .controller_debug_flag = CONTROLLER_ADV_LOST_DEBUG_BIT,                \
+    .mode = ESP_BT_MODE_CLASSIC_BT,                                      \
+    .ble_max_conn = 0,               \
+    .bt_max_acl_conn = CONFIG_BTDM_CONTROLLER_BR_EDR_MAX_ACL_CONN_EFF,     \
+    .bt_sco_datapath = CONFIG_BTDM_CTRL_BR_EDR_SCO_DATA_PATH_EFF,          \
+    .auto_latency = BTDM_CTRL_AUTO_LATENCY_EFF,                            \
+    .bt_legacy_auth_vs_evt = BTDM_CTRL_LEGACY_AUTH_VENDOR_EVT_EFF,         \
+    .bt_max_sync_conn = CONFIG_BTDM_CONTROLLER_BR_EDR_MAX_SYNC_CONN_EFF,   \
+    .magic = ESP_BT_CONTROLLER_CONFIG_MAGIC_VAL,                           \
+};
+
+static bool a2dpStart() {
+	esp_bt_controller_config_t cfg = BT_CONTROLLER_CLASSIC_CONFIG();
+	if(esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_ENABLED){
+		return true;
+	}
+	if(esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_IDLE){
+		esp_bt_controller_init(&cfg);
+		while(esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_IDLE){}
+	}
+	if(esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_INITED){
+		if (esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) {
+			log_e("BT Enable failed");
+			return false;
+		}
+	}
+	if(esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_ENABLED){
+		return true;
+	}
+	log_e("BT Start failed");
+	return false;
+}
+
 A2DP::Audio::Audio() {
 }
 
 bool A2DP::Audio::init(const char *deviceName) {
 	esp_bt_mem_release(ESP_BT_MODE_BLE);
 
-	btStart();
+	a2dpStart();
     ESP_LOGI(A2DP_AUDIO_TAG, "btStarted() said: %d", btStarted());
 
     if (esp_bluedroid_init() != ESP_OK) {
